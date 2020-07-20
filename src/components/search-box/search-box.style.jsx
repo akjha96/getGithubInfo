d3b5ac28520a7a09b26b9145b7323c89bd923ece
akjha96 <akjha96@gmail.com>
2020-07-20 22:28:51 +0530

    fixed width  of  search box

--- src/components/search-box/search-box.style.jsx
+++ src/components/search-box/search-box.style.jsx
@@ -16,6 +16,7 @@ export const InputStyle = styled.input`
   border-radius: 20px;
   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08), 0 2px 2px rgba(0, 0, 0, 0.12), 0 4px 4px rgba(0, 0, 0, 0.16), 0 8px 8px rgba(0, 0, 0, 0.2);
   height: 40px;
+  width: 200px;
   text-align: center;
   &:focus {
     outline: none;
