589af97e9903cd87d921bcc793ab6ea208dfb385
akjha96 <akjha96@gmail.com>
2020-07-20 22:09:47 +0530

    Updates

---
+++ precache-manifest.1c6dc8fd4fc996c1b80197fedbc525e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcbb8b39050e282794c59a69d6fb6aa",
+    "url": "/getGithubInfo/index.html"
+  },
+  {
+    "revision": "308c613190db0a05b4b9",
+    "url": "/getGithubInfo/static/css/main.392525b7.chunk.css"
+  },
+  {
+    "revision": "f30f0af0e9ce71838ecb",
+    "url": "/getGithubInfo/static/js/2.52311708.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/getGithubInfo/static/js/2.52311708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "308c613190db0a05b4b9",
+    "url": "/getGithubInfo/static/js/main.22dd555a.chunk.js"
+  },
+  {
+    "revision": "f4893a84cd05d1d4edb3",
+    "url": "/getGithubInfo/static/js/runtime-main.b9f2a920.js"
+  },
+  {
+    "revision": "d617763fd5c08d9196374c29974e30ab",
+    "url": "/getGithubInfo/static/media/octocat.d617763f.svg"
+  }
+]);
